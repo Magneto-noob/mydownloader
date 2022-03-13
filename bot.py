92dfb09d6984e99376f984b5a681ccdf7dfbeb1a
Magneto-noob <87361323+Magneto-noob@users.noreply.github.com>
2022-03-13 11:06:36 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -432,11 +432,11 @@ def download_video(message, video):
         logger.error(filename_cmd)
         index=1
         caption = f"/leechwatch {link} | {index}.{title}"
-        index += 1
         return 1, "", caption, quote, filename
     yt_title, path = out1.split("\n")
     if title == "":
         title = yt_title
+    index += 1
 
     download_cmd = f"{cmd} -R 25 --fragment-retries 25 --external-downloader aria2c --downloader-args 'aria2c: -x 16 -j 32'"
     st2, out2 = getstatusoutput(download_cmd)
